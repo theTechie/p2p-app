867d5b58e7554f646d12dd062dd73cd956bb4bdb
theTechie <gagan.blr@gmail.com>
2015-09-20 22:31:32 +0530

    complete source

---
+++ eval/test_lookup.js
@@ -0,0 +1,158 @@
+/**
+ * Created by gags on 12/9/15.
+ */
+var inquirer = require('inquirer'),
+    io = require('socket.io-client'),
+    fs = require('fs'),
+    path = require('path'),
+    ip = require('ip'),
+    ss = require('socket.io-stream'),
+    Server = require('socket.io'),
+    ioServer = new Server(),
+    socket = io('http://localhost:3000');
+
+var argv = require('optimist')
+    .usage('Usage: $0 -f [FILES] -p [PORT]')
+    .demand(['f', 'p'])
+    .alias('f', 'folder')
+    .describe('f', 'Folder with files to register')
+    .alias('p', 'port')
+    .describe('p', 'Port to run on')
+    .argv;
+
+var folderName = argv.folder;
+
+var files = [];
+
+var iteration = 0;
+var maxIteration = 1000;
+var totalLatency = 0;
+var filesToSearch = [];
+
+fs.readdir(folderName, function (err, list) {
+    if (err) {
+        console.log("Please provide a valid folder name !");
+        process.exit();
+    } else {
+        list.forEach(function (file, i) {
+            files.push(file);
+        });
+
+        socket.on('connect', onConnect);
+    }
+});
+
+function onConnect(message) {
+    logMessage("Connected to Index Server !");
+    //registerPeer(files, argv.port);
+}
+
+socket.on('init', function (message) {
+    logMessage(message);
+    // File Not Found Lookup
+    //filesToSearch = ['file_not_found'];
+    //testLookup();
+    filesToSearch = ['file_1k_p1','file_2k_p2','file_3k_p3','file_4k_p1','file_5k_p2','file_6k_p3','file_7k_p1','file_8k_p2','file_9k_p3', 'file_10k_p3'];
+    testLookup();
+});
+
+function testLookup() {
+    iteration++;
+    if (iteration < maxIteration) {
+        var toSearch = filesToSearch[Math.floor(Math.random() * filesToSearch.length)];
+        lookupFile(toSearch);
+    } else {
+        console.log("Lookup Latency / Lookup (ms) : ", totalLatency / maxIteration);
+        process.exit();
+    }
+}
+
+function registerPeer(files, port) {
+    socket.emit('register', { files : files, ip_port: ip.address() + ":" + port });
+}
+
+function lookupFile(fileName) {
+    socket.emit('lookup', { fileName : fileName, timestamp: Date.now() });
+}
+
+socket.on('peerList', function (response) {
+    var latency = Date.now() - response.timestamp;
+    console.log("Lookup Latency : ", latency);
+    totalLatency += latency;
+    testLookup();
+    //promptForPeerSelection(response.peerList, response.fileName);
+});
+
+function promptForPeerSelection(peerList, fileName) {
+    peerList = peerList.map(function(peer, i) { return peer.peer; });
+
+    var requestForPeerSelection = [{
+        type: "list",
+        name: "peer",
+        message: "Please select the peer you want to download from : ",
+        choices: peerList
+    }];
+
+    /* Prompt again if file was not found */
+    if (peerList.length > 0) {
+        inquirer.prompt(requestForPeerSelection, function( response ) {
+            console.log(response);
+            downloadFile(response.peer, fileName);
+            // Emit to a specific peer (how to traget to a peer ?)
+            //socket.emit('obtain', { fileName: response.fileName });
+        });
+    } else {
+        logMessage("Sorry, could not locate the file requested in any of the peers !");
+        promptForFileName();
+    }
+}
+
+function downloadFile(peer, fileName) {
+    var ioClient = io('http://' + peer, { 'forceNew': true });
+    console.log("connecting to peer : ", 'http://' + peer);
+
+    ioClient.on('connect', function () {
+        console.log("Connect to required peer for download !");
+
+        ioClient.emit('obtain', { fileName: fileName });
+    });
+
+    ioClient.on('disconnect', function () {
+        console.log("Download Complete. Disconnecting Peer !");
+    });
+
+    ss(ioClient).on('download', function (stream, data) {
+        var filename = path.basename(data.name);
+        stream.pipe(fs.createWriteStream(__dirname + '/files/' + filename));
+
+        stream.on('end', function () {
+            console.log("File Downloaded !");
+            ioClient.disconnect();
+            promptForFileName();
+        });
+    });
+}
+
+ioServer.on('connect', function (socket) {
+    logMessage("Connected to Peer : " + socket.id);
+
+    socket.on('obtain', function (response) {
+        var stream = ss.createStream();
+        ss(socket).emit('download', stream, { name: response.fileName });
+        fs.createReadStream(__dirname + '/files/' + response.fileName).pipe(stream);
+    });
+});
+
+socket.on('event', function (data) {
+    logMessage("Event: " + data);
+});
+
+socket.on('disconnect', function () {
+    logMessage("Index Server Went Down !");
+});
+
+function logMessage(message) {
+    console.log("[Client] : ", message);
+}
+
+ioServer.listen(argv.port);
---
+++ server/server.js
@@ -0,0 +1,74 @@
+/**
+ * Created by gags on 12/9/15.
+ */
+var Server = require('socket.io'),
+    io = new Server(),
+    HashTable = require('hashtable'),
+    hashtable = new HashTable();
+
+var PORT = 3000;
+
+io.on('connect', function (socket) {
+    logMessage("Client Connected : " + socket.handshake.address);
+    socket.emit('init', "Welcome to P2P Index Server !");
+
+    socket.on('register', function (response) {
+        registerPeer(socket.id, response.files, response.ip_port);
+    });
+
+    socket.on('lookup', function (response) {
+        logMessage("File Requested -> " + response.fileName);
+        var peerList = lookupFile(response.fileName);
+        socket.emit('peerList', { peerList: peerList, fileName: response.fileName, timestamp: response.timestamp });
+    });
+
+    socket.on('event', function (data) {
+        logMessage("Event: " + data);
+    });
+
+    socket.on('disconnect', function () {
+        deregisterPeer(socket.id);
+        logMessage("Client Disconnected ! ");
+    });
+});
+
+function registerPeer(peerId, files, ip_port) {
+    var peerInfo = {};
+    /* This is hardly happen as registering a peer is always 1 time*/
+    if (hashtable.has(peerId)) {
+        peerInfo = hashtable.get(peerId);
+    }
+
+    peerInfo.files = files;
+    peerInfo.ip_port = ip_port;
+    hashtable.put(peerId, peerInfo);
+
+    logMessage("Messages Registered : " + hashtable.size());
+    files.forEach(function (file, i) {
+        console.log("files registered : ", file);
+    });
+}
+
+function deregisterPeer(peerId) {
+    if (hashtable.has(peerId)) {
+        hashtable.remove(peerId);
+    }
+}
+
+/* Search for fileName across peers */
+function lookupFile(fileName) {
+    var peerList = [];
+    hashtable.forEach(function (key, value) {
+        if (value.files.indexOf(fileName) != -1) {
+            peerList.push({ peer: value.ip_port });
+        }
+    });
+    return peerList;
+}
+
+function logMessage(message) {
+    console.log("[Index Server] : ", message);
+}
+
+io.listen(PORT);
+console.log("Index Server Running at : http://localhost:" + PORT);
